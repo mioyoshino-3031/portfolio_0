fe5946c60317c4d7ecf9e31cac028ae88ed4d366
mioyoshino-3031 <mioyoshino@MacBookAir-4869.local>
2021-02-01 11:34:03 +0900

    add script dark mord

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ window.onload = function() {
     const loading = document.getElementById('loading');
     loading.classList.add('loaded')
 }
+
+//ダークモード
+// チェックボックスの取得
+const btn = document.querySelector("#switch-mode");
+
+// チェックした時の挙動
+btn.addEventListener("change", () => {
+  if (btn.checked == true) {
+    // ダークモード
+    document.body.classList.remove("light-mode");
+    document.body.classList.add("dark-mode");
+    document.getElementById("switch-mode-button").classList.remove("light-mode-button")
+    document.getElementById("switch-mode-button").classList.add("dark-mode-button")
+  } else {
+    // ライトモード
+    document.body.classList.remove("dark-mode");
+    document.body.classList.add("light-mode");
+    document.getElementById("switch-mode-button").classList.remove("dark-mode-button")
+    document.getElementById("switch-mode-button").classList.add("light-mode-button")
+  }
+});
